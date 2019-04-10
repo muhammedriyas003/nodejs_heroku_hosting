2427e54fc37427af7ef8ff8e09f2f9ee323a0b3d
muhammedriyas003 <muhammedriyas003@gmail.com>
2019-04-10 14:20:43 +0530

    m

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/login',(req,res)=>{
     var user=req.body.username;
     var pass=req.body.password;
 
-    if (user==admin && pass==1234)
+    if (user=='admin' && pass==1234)
     {
         res.send("success");
     }
